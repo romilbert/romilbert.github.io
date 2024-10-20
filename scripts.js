c1ab3fe04e22a5211fb9fac9624f3defdd1c3bcb
romilbert <romain.ilbert@hotmail.fr>
2024-10-20 18:06:30 +0200

    update

--- scripts.js
+++ scripts.js
@@ -4,24 +4,29 @@ function toggleDarkMode() {
 
 // Fonction pour détecter si un élément est visible dans le viewport
 function isInViewport(element) {
-const rect = element.getBoundingClientRect();
-return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-);
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
 }
 
 // Fonction pour révéler et animer les titres au scroll
 function revealTypingOnScroll() {
-const titles = document.querySelectorAll('.typewriter');
+    const titles = document.querySelectorAll('.typewriter');
 
-titles.forEach((title) => {
-    if (isInViewport(title)) {
-    title.classList.add('visible');
-    }
-});
+    titles.forEach((title) => {
+        if (isInViewport(title)) {
+            title.classList.add('visible');
+
+            // Une fois que l'animation de saisie est terminée, supprimer la barre de saisie
+            setTimeout(() => {
+                title.querySelector('span').classList.add('end'); // Ajoute la classe 'end' pour supprimer le curseur
+            }, 3000); // 3 secondes pour correspondre à la durée de l'animation de saisie
+        }
+    });
 }
 
 // Écouteur d'événement pour le scroll
