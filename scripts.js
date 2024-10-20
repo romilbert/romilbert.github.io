80c70d11bb83bc92d203319743f331ba8daf06de
romilbert <romain.ilbert@hotmail.fr>
2024-10-20 17:49:59 +0200

    scroll reveal

--- scripts.js
+++ scripts.js
@@ -2,30 +2,30 @@ function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
 }
 
-  // Fonction pour détecter si un élément est visible dans le viewport
-  function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
+// Fonction pour détecter si un élément est visible dans le viewport
+function isInViewport(element) {
+const rect = element.getBoundingClientRect();
+return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+);
+}
 
-  // Fonction pour révéler les titres au scroll
-  function revealOnScroll() {
-    const titles = document.querySelectorAll('.hidden-title');
+// Fonction pour révéler et animer les titres au scroll
+function revealTypingOnScroll() {
+const titles = document.querySelectorAll('.typewriter');
 
-    titles.forEach((title) => {
-      if (isInViewport(title)) {
-        title.classList.add('visible');
-      }
-    });
-  }
+titles.forEach((title) => {
+    if (isInViewport(title)) {
+    title.classList.add('visible');
+    }
+});
+}
 
-  // Écouteur d'événement pour le scroll
-  window.addEventListener('scroll', revealOnScroll);
+// Écouteur d'événement pour le scroll
+window.addEventListener('scroll', revealTypingOnScroll);
 
-  // Révéler au chargement de la page au cas où certains titres sont déjà dans la vue
-  window.addEventListener('load', revealOnScroll);
+// Révéler les titres au chargement de la page si déjà visibles
+window.addEventListener('load', revealTypingOnScroll);
