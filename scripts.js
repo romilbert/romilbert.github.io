2ad6048c9c2a0f780708809784c99f6b976985e3
romilbert <romain.ilbert@hotmail.fr>
2024-10-22 09:06:57 +0200

    update mobile mode

--- scripts.js
+++ scripts.js
@@ -2,19 +2,20 @@ function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
 }
 
-// Fonction pour forcer la position fixe sur mobile lors du scroll
+// Fonction pour changer l'opacité du bouton lors du scroll
 function handleScrollMobile() {
-    var button = document.querySelector('.top-0.right-0');
+    const button = document.querySelector('.top-0.right-0');
     
-    if (window.innerWidth <= 767) { // Si l'écran est plus petit que 767px (mobile)
-      button.style.position = "fixed"; // Forcer la position fixe
-      button.style.top = "10px";
-      button.style.right = "8px";
+    if (window.innerWidth <= 767) {
+      // Si la page est scrollée vers le bas, diminuer l'opacité
+      if (window.scrollY > 100) {
+        button.style.opacity = '0.5';
+      } else {
+        // Sinon, opacité normale
+        button.style.opacity = '1';
+      }
     }
   }
   
-  // Ajouter l'événement scroll pour mobile
+  // Appliquer l'effet de scroll uniquement sur mobile
   window.addEventListener('scroll', handleScrollMobile);
-  
-  // Assurer la position correcte dès le chargement de la page
-  window.addEventListener('load', handleScrollMobile);
