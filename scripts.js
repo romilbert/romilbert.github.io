cb0582a04368de6193e6358ab551c713c9fedc61
romilbert <romain.ilbert@hotmail.fr>
2024-10-22 09:51:41 +0200

    update mobile mode

--- scripts.js
+++ scripts.js
@@ -1,21 +1,3 @@
 function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
 }
-
-// Fonction pour changer l'opacité du bouton lors du scroll
-function handleScrollMobile() {
-    const button = document.querySelector('.dark-mode-toggle');
-    
-    if (window.innerWidth <= 767) {
-      // Si la page est scrollée vers le bas, diminuer l'opacité
-      if (window.scrollY > 100) {
-        button.style.opacity = '0.5';
-      } else {
-        // Sinon, opacité normale
-        button.style.opacity = '1';
-      }
-    }
-  }
-  
-  // Appliquer l'effet de scroll uniquement sur mobile
-  window.addEventListener('scroll', handleScrollMobile);
