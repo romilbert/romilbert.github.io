7c587d3b76a754616757c8ce9d9627dfa746ea8f
romilbert <romain.ilbert@hotmail.fr>
2024-10-22 08:53:47 +0200

    update mobile mode

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
 function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
 }
+
+// Fonction pour forcer la position fixe sur mobile lors du scroll
+function handleScrollMobile() {
+    var button = document.querySelector('.top-0.right-0');
+    
+    if (window.innerWidth <= 767) { // Si l'écran est plus petit que 767px (mobile)
+      button.style.position = "fixed"; // Forcer la position fixe
+      button.style.top = "10px";
+      button.style.right = "8px";
+    }
+  }
+  
+  // Ajouter l'événement scroll pour mobile
+  window.addEventListener('scroll', handleScrollMobile);
+  
+  // Assurer la position correcte dès le chargement de la page
+  window.addEventListener('load', handleScrollMobile);
